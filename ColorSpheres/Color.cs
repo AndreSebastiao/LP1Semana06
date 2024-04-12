0a39acada69343c44a48398a74c0e480f4c4c85c
AndreSebastiao <andresebastiao755@gmail.com>
2024-04-12 23:35:00 +0100

    Color and Sphere properties

---
+++ ColorSpheres/Color.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColorSpheres
+{
+    public class Color
+    {
+        private byte red, green, blue, alpha;
+
+        private Color(byte red, byte green, byte blue, byte alpha)
+        {
+            red = SetRed(r);
+            green = SetGreen(g);
+            blue = SetBlue(b);
+            alpha = SetAlpha(alpha);
+        }
+
+        private Color(byte red, byte green, byte blue)
+        {
+            red = SetRed(r);
+            green = SetGreen(g);
+            blue = SetBlue(b);
+            alpha = 255;
+        }
+    }
+}
